e5a9bc5f2a0166de22796ec65e6d1b1ad219d0b2
Krista <krista.coding@gmail.com>
2020-10-04 12:05:15 -0400

    update

--- javascript.js
+++ javascript.js
@@ -1,57 +1,62 @@
-$(function(){
-
-    var currentDate = (moment().format('LLLL'));
-    $("#dashboard").append(currentDate);
-
-
-
+$(function () {
 
     //When I click the search button, I am presented with current and future conditions. 
     $("button").on("click", function () {
         var searchCity = $("#searchCity").val();
         $("#searchCity").val("");
         searchWeather(searchCity);
+        fiveDayForecast(searchCity);
     });
 
     function searchWeather(searchCity) {
 
-    var queryURL = "http://api.openweathermap.org/data/2.5/weather?q=" + searchCity + "&APPID=0c7291aca640c07cf04da224af9c3247&units=imperial";
-    console.log(queryURL);
-    console.log(searchCity);
+        var queryURL = "https://api.openweathermap.org/data/2.5/weather?q=" + searchCity + "&APPID=0c7291aca640c07cf04da224af9c3247&units=imperial";
+        console.log(searchCity);
+        console.log(queryURL);
+        localStorage.setItem("history", searchCity);
 
-    $.ajax({
-        url: queryURL,
-        method: "GET"
-    })
-        .then(function (response) {
+        $.ajax({
+            url: queryURL,
+            method: "GET"
+        })
+            .then(function (response) {
 
-            $("#currentCity").text(searchCity);
+                $("#currentCity").text(searchCity) + " " + response.list.weather.icon;
+                
+                var temperature = $("<p>").text("Temperature: " + response.main.temp + "°F");
 
-            var temperature = $("<p>").text("Temperature: " + response.main.temp + "°F");
+                $('#currentCity').append(temperature);
 
-            $('#currentCity').append(temperature);
+                var humidity = $("<p>").text("Humidity: " + response.main.humidity + "%");
 
-            var humidity = $("<p>").text("Humidity: " + response.main.humidity + "%");
+                temperature.append(humidity);
 
-            temperature.append(humidity);
+                var windSpeed = $("<p>").text("Wind Speend: " + response.wind.speed + "MPH");
 
-            var windSpeed = $("<p>").text("Wind Speend: " + response.wind.speed + "MPH");
+                humidity.append(windSpeed);
 
-            humidity.append(temperature);
-
-        });
-        
+            });
     
     }
 
-    var history = JSON.parse(window.localStorage.getItem("history")) || [];
-    if (history.length > 0) {
-        searchWeather(history[history.length - 1]);
-    }
+    function fiveDayForecast(searchCity) {
+        console.log(searchCity);
 
-    for ( var i = 0; i > history.length; i++){
-        historyRow(history[i]); 
-    }
+        var queryURL = "https://api.openweathermap.org/data/2.5/forecast?q=" + searchCity + "&appid=0c7291aca640c07cf04da224af9c3247&units=imperial";
+
+        console.log(queryURL);
+
+        $.ajax({
+            url: queryURL,
+            method: "GET"
+        })
+            .then(function (response) {
+
+                var day1 = $("<row>").text(response.list.dt_txt);
+
+                $('forecast').append(day1);
+            });
         
     }
+
 });
